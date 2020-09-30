a01f9031b179f6e9cee26d8901990220ad2efdc9
Aman Jhurani <47776459+amanjhurani@users.noreply.github.com>
2020-10-01 02:27:31 +0530

    Update secure.py
    Gave another try on wrong input instead of exit.

--- secure.py
+++ secure.py
@@ -12,59 +12,62 @@ def decrypt(ciphertext):
 	cleartext = obj2.decrypt(ciphertext)
 	return cleartext
 
-print("\t\t Welcome! \n")
-choice1 = int(input("Enter the choice: 1. Encrypt 2.Decrypt 3. Exit"))
+	
+def options(choice1):
+	if choice1 == 1:
+		infile = input("Enter the file name which is to be encrypted: ")
+		outfile = input("Enter the location to store the secured data: ")
 
-if choice1 == 1:
-	infile = input("Enter the file name which is to be encrypted: ")
-	outfile = input("Enter the location to store the secured data: ")
+		file1 = open(infile, 'r')
+		data = file1.read()
+		ciphertext = encrypt(data)
 
-	file1 = open(infile, 'r')
-	data = file1.read()
-	ciphertext = encrypt(data)
+		file2 = open(outfile,'w')
+		file2.write(str(ciphertext))
 
-	file2 = open(outfile,'w')
-	file2.write(str(ciphertext))
+		print("Encryption Succcessful!")
 
-	print("Encryption Succcessful!")
+		file2.close()
+		file1.close()	
+		exit()
 
-	file2.close()
-	file1.close()	
-	exit()
+	elif choice1 == 2:
+		infile = input("Enter the filename which is to be decrypted: ")
+		outfile = input("Enter the file where the cleartext has to be saved: ")
 
-elif choice1 == 2:
-	infile = input("Enter the filename which is to be decrypted: ")
-	outfile = input("Enter the file where the cleartext has to be saved: ")
+		file1 = open(infile, 'r')
+		data = file1.read()
+		cleartext = decrypt(str.encode(data))
 
-	file1 = open(infile, 'r')
-	data = file1.read()
-	cleartext = decrypt(str.encode(data))
+		print(cleartext)
 
-	print(cleartext)
+		file2 = open(outfile,'w')
+		file2.write(str(cleartext))
 
-	file2 = open(outfile,'w')
-	file2.write(str(cleartext))
+		print("Decryption Succcessful!")
 
-	print("Decryption Succcessful!")
+		file2.close()
+		file1.close()
+		exit()
 
-	file2.close()
-	file1.close()
-	exit()
+	elif choice1 == 3:
+		print("Thank you!")
+		exit()
 
-elif choice1 == 3:
-	print("Thank you!")
-	exit()
-
-else:
-	print("Enter the valid choice!")
-	exit()
+	else:
+		print("Enter the valid choice!")
+		options(choice1)
 
 
-# from Crypto.Cipher import AES
-# obj = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
-# message = "Hemang"
-# ciphertext = obj.encrypt(message)
-# print(ciphertext)
-# obj2 = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
-# cleartext = obj2.decrypt(ciphertext)
-# print(cleartext)
+	# from Crypto.Cipher import AES
+	# obj = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+	# message = "Hemang"
+	# ciphertext = obj.encrypt(message)
+	# print(ciphertext)
+	# obj2 = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+	# cleartext = obj2.decrypt(ciphertext)
+	# print(cleartext)
+if __name__=="__main__":
+	print("\t\t Welcome! \n")
+	choice1 = int(input("Enter the choice: 1. Encrypt 2.Decrypt 3. Exit"))
+	options(choice1)
